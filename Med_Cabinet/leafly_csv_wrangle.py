b1a70128873f302f56aae0ac5004956fbfb63e77
John Dailey <johnjdailey@gmail.com>
2020-06-19 20:35:31 -0400

    Wrangle Leafly effects, new csv/names, update readme

--- Med_Cabinet/leafly_csv_wrangle.py
+++ Med_Cabinet/leafly_csv_wrangle.py
@@ -1,15 +1,20 @@
-# leafly_csv_wrangle.py
+# Leafly_csv_Wrangle.py
 
 # First wrangle to get unique effects for front end user survey and ML use
+ 
+# Second wrangle to strip "[]"" from list of Effects in Effects column values
+# and replace "," with " " in attempt for better neural networking fit.
 
 
+
+# Imports
 
 import pandas as pd
 
 
 # Import Leafly csv
 
-file_name = r"C:\Users\johnj\OneDrive\Documents\Lambda\BuildWeek3\data-science\Med_Cabinet\cannabis.csv"
+file_name = r"C:\Users\johnj\OneDrive\Documents\Lambda\BuildWeek3\data-science\Med_Cabinet\data\Leafly.csv"
 
 df = pd.read_csv(file_name)
 
@@ -17,6 +22,8 @@ df = pd.read_csv(file_name)
 
 #print(df.head())
 
+
+# First wrangle for unique effects
 
 # Check type of Effects column data
 
@@ -48,21 +55,50 @@ def unique(effects):
     for x in unique_list_of_effects: 
         print(x)
 
-print(unique(stripped_effects)) 
+# Commented out as job is done, and on to second wrangle
+
+#print(unique(stripped_effects))
 
 # 13 Unique Effects
 
-#Dry Mouth
-#Euphoric
-#Happy
-#Relaxed
-#Focused
-#Energetic
-#Sleepy
-#Talkative
-#Tingly
-#Aroused
-#Giggly
-#Creative
-#Hungry
-#Uplifted
+# Dry Mouth - Not desireable, not included in 13
+# Euphoric
+# Happy
+# Relaxed
+# Focused
+# Energetic
+# Sleepy
+# Talkative
+# Tingly
+# Aroused
+# Giggly
+# Creative
+# Hungry
+# Uplifted
+
+
+# Second wrangle
+
+# Make the stripped, split and replace in Effects column persist (uses strip and split from Wrangle 1)
+
+df["Effects"] = df["Effects"].str.replace(","," ").astype(str)
+
+# Check type after strip and split, which is <class 'pandas.core.series.Series'>
+
+print(type(df['Effects']))
+
+# Verify changes with printout to terminal
+
+print(df['Effects'].head())
+
+# Set pandas option to show all columns in final printout verification
+
+pd.set_option('display.max_columns', None)
+
+print(df.head())
+
+# Export csv for testing in neural network baseline
+
+file_name = r"C:\Users\johnj\OneDrive\Documents\Lambda\BuildWeek3\data-science\Med_Cabinet\data\Leafly_nolistcommas.csv"
+
+df.to_csv(file_name, sep='\t', encoding='utf-8')
