203aebad78e1c2d409b906b26db67525375d88ce
John Dailey <johnjdailey@gmail.com>
2020-06-26 03:59:24 -0400

    web_app

--- web_app/routes/Directory.py
+++ web_app/routes/Directory.py
@@ -4,7 +4,10 @@
 
 # Import
 
+from os import path
+import pandas as pd
 from flask import Blueprint, render_template
+from werkzeug.routing import BaseConverter
 
 
 # Make Blueprint for __init__.py
@@ -12,22 +15,55 @@ from flask import Blueprint, render_template
 Directory = Blueprint("Directory", __name__)
 
 
+# Import Leafly csv
+
+file_name = path.join(path.dirname(__file__), "Leafly.csv")
+
+df = pd.read_csv(file_name)
+
+strains = df['Strain']
+
+
 # App Welcome Page
 
 @Directory.route('/')
-def home():
-    return render_template("home.html", message = "DS Med Cabinet API using natural language processing to recommend the best cannabis strains to Med Cabinet members.")
+def index():
+    return render_template("home.html", message = "DS Med Cabinet API, using natural language processing to recommend the best cannabis strains to Med Cabinet members.")
 
 
 # Strain JSON Page
 
 @Directory.route("/strainjson")
-def json():
+def strainjson():
     return render_template("json.html")
 
 
 # Strain Table Page
 
 @Directory.route("/straintable")
-def df():
+def straintable():
     return render_template("df.html")
+
+
+# Custom converter
+
+class ListConverter(BaseConverter):
+
+    def to_python(self, value):
+        return value.split('+')
+
+    def to_url(self, values):
+        return '+'.join(BaseConverter.to_url(value)
+                        for value in values)
+
+
+# Flask Url-converter
+
+@Directory.route('/<strain>')
+def strain_url(strain):
+    """Show all of the posts for the given strain."""
+    strain = []
+    for strain in strains:
+        strain = strain in strains
+
+    return render_template('json.html', strain=strain)
